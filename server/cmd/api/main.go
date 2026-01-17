f4cb0bfaf270f4602bef8cc7ab2742b5e4b0692e
Aswanth P R <aswanth.pr@jethur.com>
2026-01-17 11:39:02 +0530

    first commit with go folders

---
+++ server/cmd/api/main.go
@@ -0,0 +1,19 @@
+﻿package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	log.Println("server is running in  80000")
+	router.Run()
+}
